bd269a75273339d64888def5eb6978637f9b55ec
Data Analytics Dojo <61879325+DataAnalyticsDojo@users.noreply.github.com>
2020-03-22 15:24:31 -0500

    Added post deploy script to seed business calendar.

---
+++ DataWarehouse/PostDeploymentScript.sql
@@ -0,0 +1,28 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+--
+--Seed the Business Calendar if it is empty
+--
+DECLARE @recordCount INT;
+
+SELECT @recordCount = COUNT(*) 
+FROM [Stage].[BusinessCalendar];
+
+
+IF (@recordCount = 0)
+BEGIN
+    INSERT INTO [Stage].[BusinessCalendar] 
+    VALUES 
+    ('12/24/2018', 1, 'Christmas Eve', 1),
+    ('12/25/2018', 1, 'Christmas', 0);
+END;    
