f3d094565db63465799dcfad9ca9bf16ebe76910
Data Analytics Dojo <61879325+DataAnalyticsDojo@users.noreply.github.com>
2020-03-24 07:21:47 -0500

    Update usp_LoadDimTime.sql

--- DataWarehouse/dbo/Stored Procedures/usp_LoadDimTime.sql
+++ DataWarehouse/dbo/Stored Procedures/usp_LoadDimTime.sql
@@ -1,7 +1,7 @@
 ﻿
 CREATE PROCEDURE [dbo].[usp_LoadDimTime]
-	@BeginTime					DATETIME = '1/1/1900 00:00',
-	@EndTime					DATETIME = '1/1/1900 23:59',
+	@BeginTime			DATETIME = '1/1/1900 00:00',
+	@EndTime			DATETIME = '1/1/1900 23:59',
 	@InsertUnknownTimeRecord	BIT = 1
 AS
 BEGIN
@@ -39,24 +39,24 @@ BEGIN
 				[IsUnknownTime]
 			)
 			VALUES (
-				CAST(FORMAT(@BeginTime,'HHmm') AS INT),						--[TimeKey]
-				CAST(@BeginTime AS TIME),									--[Time]
+				CAST(FORMAT(@BeginTime,'HHmm') AS INT),					--[TimeKey]
+				CAST(@BeginTime AS TIME),						--[Time]
 				CAST(DATEPART(HOUR, @BeginTime) AS TINYINT),				--[HourNumber]
 				'Hour ' + FORMAT(DATEPART(HOUR, @BeginTime), '00'),			--[HourNameLong]
-				FORMAT(DATEPART(HOUR, @BeginTime), '00'),					--[HourNameShort]
+				FORMAT(DATEPART(HOUR, @BeginTime), '00'),				--[HourNameShort]
 				CAST(DATEPART(MINUTE, @BeginTime) AS TINYINT),				--[MinuteNumber]
-				'Minute ' + FORMAT(DATEPART(MINUTE, @BeginTime), '00'),		--[MinuteNameLong]
-				FORMAT(DATEPART(MINUTE, @BeginTime), '00'),					--[MinuteNameShort]
+				'Minute ' + FORMAT(DATEPART(MINUTE, @BeginTime), '00'),			--[MinuteNameLong]
+				FORMAT(DATEPART(MINUTE, @BeginTime), '00'),				--[MinuteNameShort]
 				--
 				FORMAT(CASE ((DATEPART(HOUR, @BeginTime) % 12)) WHEN 0 THEN 12 ELSE ((DATEPART(HOUR, @BeginTime) % 12)) END, '00') + 
 					':' + 
 					 FORMAT(DATEPART(MINUTE, @BeginTime), '00') +
-					':00',													--[TimeNameStandard]
+					':00',								--[TimeNameStandard]
 				--
 				FORMAT(DATEPART(HOUR, @BeginTime), '00') + 
 					 ':' + 
 					 FORMAT(DATEPART(MINUTE, @BeginTime), '00') +
-					 ':00',													--[TimeNameMilitary]
+					 ':00',								--[TimeNameMilitary]
 				--
 				0															--[IsUnknownTime]
 			);
@@ -73,31 +73,31 @@ BEGIN
 	--
 	IF @InsertUnknownTimeRecord = 1 AND NOT EXISTS (SELECT 1 FROM [dbo].[DimTime] WHERE [TimeKey] = -1) 
 	BEGIN
-			INSERT INTO [dbo].[DimTime] (
-				[TimeKey],
-				[Time],
-				[HourNumber],
-				[HourNameLong],
-				[HourNameShort],
-				[MinuteNumber],
-				[MinuteNameLong],
-				[MinuteNameShort],
-				[TimeNameStandard],
-				[TimeNameMilitary],
-				[IsUnknownTime]
-			)
-			VALUES (
-				-1,															--[TimeKey]
-				NULL,														--[Time]
-				-1,															--[HourNumber]
-				'Unknown',													--[HourNameLong]
-				'Unknown',													--[HourNameShort]
-				-1,															--[MinuteNumber]
-				'Unknown',													--[MinuteNameLong]
-				'Unknown',													--[MinuteNameShort]
-				'Unknown',													--[TimeNameStandard]
-				'Unknown',													--[TimeNameMilitary]
-				1															--[IsUnknownTime]
-			);
+		INSERT INTO [dbo].[DimTime] (
+			[TimeKey],
+			[Time],
+			[HourNumber],
+			[HourNameLong],
+			[HourNameShort],
+			[MinuteNumber],
+			[MinuteNameLong],
+			[MinuteNameShort],
+			[TimeNameStandard],
+			[TimeNameMilitary],
+			[IsUnknownTime]
+		)
+		VALUES (
+			-1,															--[TimeKey]
+			NULL,														--[Time]
+			-1,															--[HourNumber]
+			'Unknown',													--[HourNameLong]
+			'Unknown',													--[HourNameShort]
+			-1,															--[MinuteNumber]
+			'Unknown',													--[MinuteNameLong]
+			'Unknown',													--[MinuteNameShort]
+			'Unknown',													--[TimeNameStandard]
+			'Unknown',													--[TimeNameMilitary]
+			1															--[IsUnknownTime]
+		);
 	END;
 END;
